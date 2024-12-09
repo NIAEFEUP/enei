e964688ecfb373e3aa82c2919cdc238519cc957e
André Lima <me@limwa.pt>
2024-12-09 23:36:53 +0000

    feat: initial scaffold finished

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A flake for development of ENEI's applications";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    formatter.${system} = pkgs.alejandra;
+
+    devShells.${system}.website = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_22
+        corepack_22
+      ];
+    };
+  };
+}
