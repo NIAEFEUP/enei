a95bda85f78441c2c00f7b04717053c17d786659
Jose Sousa <2409jmsousa@gmail.com>
2024-12-21 13:31:53 +0000

    feat: created product and order models

---
+++ website/app/models/order.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Order extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare user_id: number
+
+  @column()
+  declare product_id: number
+
+  @column()
+  declare quantity: number
+  
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ website/database/migrations/1734776385300_create_orders_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'orders'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').notNullable()
+      table.integer('product_id').notNullable()
+      table.integer('quantity').notNullable()
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
