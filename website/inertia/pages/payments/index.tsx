4f92b36bba7c70dd176405d2f93088f51c324f01
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-12-22 00:00:52 +0000

    feat: improved looks, not sure about the border on the images

--- website/inertia/pages/payments/index.tsx
+++ website/inertia/pages/payments/index.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { useState } from 'react'
 import {
   Card,
   CardContent,
@@ -13,18 +12,21 @@ import { Label } from '~/components/ui/label'
 import { Input } from '~/components/ui/input'
 import { Button } from '~/components/ui/button'
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '~/components/ui/collapsible'
-import { motion, AnimatePresence } from 'framer-motion'
+import { Separator } from '~/components/ui/separator'
+import { useState } from 'react'
+import { RadioGroup, RadioGroupItem } from '~/components/ui/radio-group'
 
 const item = {
   title: 'Bilhete - Com alojamento',
   description:
     'Inclui: Pequenos-almoços, almoços e jantares durante o período do evento<br>Acesso a coffee breaks e sessão de cocktails\nAcesso a workshops, palestras e outros\nAcesso a festas noturnas e outras atividades recreativas (exceto Rally Tascas)\nAlojamento em Pavilhão',
   price: 35,
-  img: 'favicon.svg',
+  image: 'favicon.svg',
 }
 
 export default function TicketSalePage() {
-  const [showBillingAddress, setShowBillingAddress] = useState(false)
+  const [enableBillingAddress, setEnableBillingAddress] = useState(true)
+  const [paymentMethod, setPaymentMethod] = useState<string>('mbway')
 
   return (
     <div className="container mx-auto p-4">
@@ -34,62 +36,93 @@ export default function TicketSalePage() {
           <CardDescription>Revê o teu bilhete e procede para o pagamento</CardDescription>
         </CardHeader>
         <CardContent className="space-y-6">
-          {/* Ticket Information */}
-          <div className="flex items-start space-x-4">
-            <img src={`/${item.img}`} alt="Item" className="w-16 h-16" />
-            <div>
-              <h3 className="text-lg font-semibold">{item.title}</h3>
-              <p className="text-sm text-gray-500">{item.description}</p>
-              <p className="text-lg font-bold mt-2">{item.price.toFixed(2)}€</p>
+          <section>
+            <h2 className="text-xl font-semibold mb-4">1. Revê a tua compra</h2>
+            <div className="flex items-start space-x-4">
+              <img
+                src={item.image}
+                alt={item.title}
+                className="w-[150px] h-[100px] object-cover rounded-md"
+              />
+              <div>
+                <h3 className="text-lg font-semibold">{item.title}</h3>
+                <p className="text-sm text-gray-500">{item.description}</p>
+                <p className="text-lg font-bold mt-2">{item.price.toFixed(2)}€</p>
+              </div>
             </div>
-          </div>
+          </section>
 
-          {/* Toggle for showing the form */}
-          <div className="flex items-center space-x-2">
-            <Checkbox
-              id="billingAddress"
-              checked={showBillingAddress}
-              onCheckedChange={(checked) => setShowBillingAddress(checked as boolean)}
-            />
-            <Label htmlFor="billingAddress">Incluir dados de faturação</Label>
-          </div>
+          <Separator />
 
-          {/* Billing information form */}
-          <AnimatePresence>
-            {showBillingAddress && (
-              <motion.div
-                initial={{ opacity: 0, height: 0 }}
-                animate={{ opacity: 1, height: 'auto' }}
-                exit={{ opacity: 0, height: 0 }}
-                transition={{ duration: 0.3 }}
-                className="overflow-hidden"
-              >
-                <Collapsible open={showBillingAddress}>
-                  <CollapsibleContent className="space-y-4">
-                    <div className="grid gap-4">
-                      <div className="grid gap-2">
-                        <Label htmlFor="name">Nome</Label>
-                        <Input id="name" placeholder="Nome completo" />
-                      </div>
-                      <div className="grid gap-2">
-                        <Label htmlFor="vat">NIF</Label>
-                        <Input id="vat" placeholder="Número de identificação fiscal" />
-                      </div>
-                      <div className="grid gap-2">
-                        <Label htmlFor="address">Morada</Label>
-                        <Input id="address" placeholder="Insere a tua morada" />
-                      </div>
-                    </div>
-                  </CollapsibleContent>
-                </Collapsible>
-              </motion.div>
-            )}
-          </AnimatePresence>
+          <section>
+            {/* Checkbox */}
+            <h2 className="text-xl font-semibold mb-4">2. Dados de faturação</h2>
+            <div className="flex items-center space-x-2 mb-4">
+              <Checkbox
+                id="billingAddress"
+                checked={enableBillingAddress}
+                onCheckedChange={(checked) => setEnableBillingAddress(checked as boolean)}
+              />
+              <Label htmlFor="billingAddress">Incluir dados de faturação</Label>
+            </div>
+
+            {/* Billing information form */}
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="name">Nome</Label>
+                <Input id="name" placeholder="Nome completo" disabled={!enableBillingAddress} />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="vat">NIF</Label>
+                <Input
+                  id="vat"
+                  placeholder="Número de identificação fiscal"
+                  disabled={!enableBillingAddress}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="address">Morada</Label>
+                <Input
+                  id="address"
+                  placeholder="Insere a tua morada"
+                  disabled={!enableBillingAddress}
+                />
+              </div>
+            </div>
+          </section>
+          <Separator />
+          <section>
+            <h2 className="text-xl font-semibold mb-4">3. Método de pagamento</h2>
+            <RadioGroup value={paymentMethod} onValueChange={setPaymentMethod}>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="mbway" id="mbway" />
+                <Label htmlFor="mbway">
+                  <img
+                    src="/images/mbway.svg"
+                    alt="MB Way"
+                    className={`w-20 h-20 m-1 p-1 ${paymentMethod === 'mbway' ? 'border-2 rounded-lg border-white' : ''}`}
+                  />
+                </Label>
+              </div>
+              <div className="flex items-center space-x-2 mt-2">
+                <RadioGroupItem value="atm" id="atm" />
+                <Label htmlFor="atm">
+                  <img
+                    src="/images/multibanco.svg"
+                    alt="Referência Multibanco"
+                    className={`w-16 h-16 p-1 ${paymentMethod === 'atm' ? 'border-2 rounded-lg border-white' : ''}`}
+                  />
+                </Label>
+              </div>
+            </RadioGroup>
+          </section>
+
+          <Separator />
+
+          <Button onClick={() => {}} disabled={!paymentMethod} className="w-full">
+            Pagar
+          </Button>
         </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Pagar com referência multibanco</Button>
-          <Button>Pagar com MBWay</Button>
-        </CardFooter>
       </Card>
     </div>
   )
