c0d0f512e0bd8af3b30ed7e362bf0e5246757985
Tomás Palma <up202108880@edu.fe.up.pt>
2025-01-08 23:02:00 +0000

    chore: remove unsued import

--- website/inertia/pages/login.tsx
+++ website/inertia/pages/login.tsx
@@ -5,7 +5,7 @@ import { Input } from '~/components/ui/input'
 import { Label } from '~/components/ui/label'
 import { Separator } from '~/components/ui/separator'
 import { useError } from '~/hooks/use_error'
-import { router, useForm } from '@inertiajs/react'
+import { useForm } from '@inertiajs/react'
 import { cn } from '~/lib/utils'
 
 export default function Login() {
