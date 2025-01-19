977ef0a1e3297f1f3c03680c9289f3aaa2cba06c
Hugo Almeida <1241273@isep.ipp.pt>
2025-01-19 15:43:57 +0000

    feat: added signup models

--- website/app/models/profile.ts
+++ website/app/models/profile.ts
@@ -5,6 +5,7 @@ export default class Profile extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  // General Info
   @column()
   declare firstName: string
 
@@ -15,10 +16,34 @@ export default class Profile extends BaseModel {
   declare age: number
 
   @column()
-  declare school: School
+  declare phoneNumber: string
+
+  // Student Info
+  @column()
+  declare year: number
 
   @column()
-  declare phoneNumber: string
+  declare isComplete: boolean
+
+  // Logistics Info
+  @column()
+  declare foodRestrictions: string
+
+  @column()
+  declare isVegetarian: boolean
+
+  @column()
+  declare transportation: string
+
+  // Communication Info
+  @column()
+  declare heardOf: string
+
+  @column()
+  declare whyAttend: string
+
+  @column()
+  declare previousExperience: string
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
