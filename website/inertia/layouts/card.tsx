234dd4573c52e80456c96c354421361df6a9c3b5
André Lima <me@limwa.pt>
2025-01-22 13:16:09 +0000

    Update card.tsx

--- website/inertia/layouts/card.tsx
+++ website/inertia/layouts/card.tsx
@@ -1,7 +1,7 @@
 export default function CardLayout({ children }: { children?: React.ReactNode }) {
   return (
     <div className="flex items-center justify-center w-full mt-24">
-      <div className="flex flex-col gap-6 max-w-sm">{children}</div>
+      <div className="flex flex-col gap-6 max-w-sm px-4">{children}</div>
     </div>
   )
 }
