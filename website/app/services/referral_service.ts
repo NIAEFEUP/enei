12c3191d2f74ae4bada7a02bc0f6f7c39a7f2c51
AvilaAndre <avila_andre14@hotmail.com>
2025-01-22 15:38:42 +0000

    refactor: add type to HashIdService

--- website/app/services/referral_service.ts
+++ website/app/services/referral_service.ts
@@ -3,11 +3,11 @@ import Hashids from 'hashids'
 export default class HashIdService {
   static hashIds = new Hashids('', 8)
 
-  static encode(id: number) {
+  static encode(id: number): string {
     return HashIdService.hashIds.encode(id)
   }
 
-  static decode(hashId: string) {
-    return HashIdService.hashIds.decode(hashId)[0]
+  static decode(hashId: string): number {
+    return HashIdService.hashIds.decode(hashId)[0] as number
   }
 }
