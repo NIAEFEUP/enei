6a13f39216b9f17c3a924c17a729f48bbe43a5b4
Jose Sousa <2409jmsousa@gmail.com>
2025-02-04 18:58:38 +0000

    fix: fixed stock

--- website/database/seeders/1_product_seeder.ts
+++ website/database/seeders/1_product_seeder.ts
@@ -7,7 +7,7 @@ export default class ProductSeeder extends BaseSeeder {
       description:
         'Inclui:<br />• Pequenos-almoços, almoços e jantares durante o período do evento<br />• Acesso a coffee breaks e sessão de cocktails<br />• Acesso a workshops, palestras e outros<br />• Acesso a festas noturnas e outras atividades recreativas (exceto Rally Tascas) <br />• Alojamento em Pavilhão',
       price: 0.01,
-      stock: 150,
+      stock: 100,
       currency: 'EUR',
       max_order: 1,
       productGroupId: 1,
