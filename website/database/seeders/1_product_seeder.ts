d58f1d30f34a52aa0d395a9c76bbc386212fad13
André Lima <me@limwa.pt>
2025-02-08 13:57:00 +0000

    fix: data on seeders for production

--- website/database/seeders/1_product_seeder.ts
+++ website/database/seeders/1_product_seeder.ts
@@ -3,10 +3,10 @@ import Product from '#models/product'
 export default class ProductSeeder extends BaseSeeder {
   public async run() {
     await Product.create({
-      name: 'Bilhete - Com Alojamento',
+      name: 'Bilhete Early Bird - Com Alojamento',
       description:
         'Inclui:<br />• Pequenos-almoços, almoços e jantares durante o período do evento<br />• Acesso a coffee breaks e sessão de cocktails<br />• Acesso a workshops, palestras e outros<br />• Acesso a festas noturnas e outras atividades recreativas (exceto Rally Tascas) <br />• Alojamento em Pavilhão',
-      price: 0.01,
+      price: 35,
       stock: 100,
       currency: 'EUR',
       max_order: 1,
@@ -14,10 +14,10 @@ export default class ProductSeeder extends BaseSeeder {
       image: '/favicon.svg',
     })
     await Product.create({
-      name: 'Bilhete - Sem Alojamento',
+      name: 'Bilhete Early Bird - Sem Alojamento',
       description:
         'Inclui:<br />• Pequenos-almoços, almoços e jantares durante o período do evento<br />• Acesso a coffee breaks e sessão de cocktails<br />• Acesso a workshops, palestras e outros<br />• Acesso a festas noturnas e outras atividades recreativas (exceto Rally Tascas)',
-      price: 0.01,
+      price: 30,
       stock: 50,
       currency: 'EUR',
       max_order: 1,
