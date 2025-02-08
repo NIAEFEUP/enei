ac1c0fe0d79bf36ae1c61629b6589d3e58270eb3
André Lima <me@limwa.pt>
2025-02-08 17:06:27 +0000

    fix: typo

--- website/inertia/pages/auth/forgot-password/reset/page.tsx
+++ website/inertia/pages/auth/forgot-password/reset/page.tsx
@@ -23,11 +23,11 @@ export default function ForgotPassword() {
   }
 
   return (
-    <BaseLayout title="Repôr palavra-passe">
+    <BaseLayout title="Repor palavra-passe">
       <CardLayout>
         <Card className={cn(oauthError && 'border-2 border-red-600')}>
           <CardHeader>
-            <CardTitle className="text-2xl">Repôr palavra-passe</CardTitle>
+            <CardTitle className="text-2xl">Repor palavra-passe</CardTitle>
             <CardDescription>
               Introduz a tua nova palavra-passe
             </CardDescription>
