f832dd16d7bade7add4981d3595eb7cf2fdc63c1
André Lima <me@limwa.pt>
2025-02-10 00:44:59 +0000

    fix: remove console logs

--- website/start/events.ts
+++ website/start/events.ts
@@ -1,5 +1,5 @@
 import UserCreated from '#events/user_created'
-import UserEmailVerified from '#events/user_email_verified'
+// import UserEmailVerified from '#events/user_email_verified'
 import UserRequestedVerificationEmail from '#events/user_requested_verification_email'
 import emitter from '@adonisjs/core/services/emitter'
 
@@ -8,4 +8,4 @@ const SendVerificationEmail = () => import('#listeners/send_verification_email')
 emitter.on(UserCreated, [SendVerificationEmail, 'handle'])
 emitter.on(UserRequestedVerificationEmail, [SendVerificationEmail, 'handle'])
 
-emitter.on(UserEmailVerified, (ev) => console.log(ev))
+// emitter.on(UserEmailVerified, (ev) => console.log(ev))
