8156658eaf0373744cf3796b1520a986b1262bf9
Jose Sousa <2409jmsousa@gmail.com>
2025-02-11 23:45:42 +0000

    fix: error code

--- website/app/exceptions/referrals_disabled_exception.ts
+++ website/app/exceptions/referrals_disabled_exception.ts
@@ -1,7 +1,7 @@
 import { Exception } from '@adonisjs/core/exceptions'
 
 export default class ReferralsDisabledException extends Exception {
-  static status = 500
+  static status = 403
   static code = 'E_REFERRAL_DISABLED'
   static message = 'Referrals not open yet'
   static help = 'The referrals feature is currently disabled'
