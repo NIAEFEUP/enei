064b8a16d9f266a9d718682b9aead114590316c0
André Lima <me@limwa.pt>
2025-02-24 16:20:15 +0000

    fix: show out of stock indicator

--- website/app/models/product.ts
+++ website/app/models/product.ts
@@ -1,5 +1,8 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
+import type { ModelAttributes } from '@adonisjs/lucid/types/model';
+
+export type SerializedProduct = ModelAttributes<Product>;
 
 export default class Product extends BaseModel {
   @column({ isPrimary: true })
