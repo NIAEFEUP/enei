33428fcc8382ca050dcaf172ea758088d1c75279
AvilaAndre <avila_andre14@hotmail.com>
2025-02-25 12:29:18 +0000

    feat: show past editions badges

--- website/inertia/pages/profile/page.tsx
+++ website/inertia/pages/profile/page.tsx
@@ -2,19 +2,33 @@ import { InferPageProps } from '@adonisjs/inertia/types'
 import ProfilesController from '#controllers/profiles_controller'
 import ParticipantProfile from '#models/participant_profile'
 import { Card } from '~/components/ui/card'
-import { Button } from '~/components/ui/button'
+import { Button, buttonVariants } from '~/components/ui/button'
 import Page from '~/components/common/page'
 import Container from '~/components/common/containers'
 import { getUniversityById } from '~/lib/enei/signup/universities'
-import { Download, EyeOff, User, Github, Instagram, Linkedin, Globe, QrCode, LucideProps } from 'lucide-react'
+import { Download, EyeOff, User, Github, Instagram, Linkedin, Globe, QrCode, LucideProps, Pencil } from 'lucide-react'
 import { Dialog, DialogContent, DialogTrigger } from '~/components/ui/dialog'
 import { QRCodeSVG } from 'qrcode.react';
 import { useEffect, useState } from 'react'
+import { Link } from '@tuyau/inertia/react'
+import { cn } from '~/lib/utils'
+import { Badge } from '~/components/ui/badge'
+import editions from '#data/enei/editions.json' with { type: 'json' }
+import { Option } from '~/components/ui/multiple-selector'
 
 interface SocialIconProps {
   icon: React.FC<LucideProps>;
   link: string;
 }
+
+const ENEI_EDITIONS: Option[] = editions
+  .sort((a, b) => b.year - a.year)
+  .map(({ year, location }) => {
+    return {
+      label: location + ', ' + year.toString(),
+      value: year.toString(),
+    }
+  })
 
 function SocialIcon({ icon: Icon, link }: SocialIconProps) {
   return (
@@ -33,18 +47,28 @@ export default function ProfilePage(props: InferPageProps<ProfilesController, 'i
     setWindowHref(window.location.href)
   })
 
+  const profileEditions = ENEI_EDITIONS.filter((edition) => profile.attendedBeforeEditions.includes(edition.value)).map((edition) => edition.label)
+
   return (
     <Page title="Perfil" className="bg-enei-beige text-white">
       <Container>
         <section className="relative flex flex-col gap-8 md:justify-between z-10">
           <Card className='p-4 flex flex-col gap-4'>
-            <h3 className='text-2xl'>Perfil do Participante</h3>
+            <div className='flex flex-row justify-between gap-8'>
+              <h3 className='text-2xl'>Perfil do Participante</h3>
+              {isUser &&
+                <Link route="pages:profile.edit" className={cn(buttonVariants({ variant: 'default' }), 'w-fit')}>
+                  <Pencil />
+                  <p className=''>Editar</p>
+                </Link>
+              }
+            </div>
 
             <section className='grid md:grid-cols-[auto_1fr] items-center gap-4 md:gap-8'>
               <div className='size-fit rounded-sm bg-enei-beige mx-auto md:mx-0'>
                 <User className='w-48 h-48' />
               </div>
-              <div className='h-full flex flex-col gap-2 justify-between py-0 md:py-4 text-center md:text-start'>
+              <div className='h-full flex flex-col gap-2 justify-between text-center md:text-start'>
                 <p className='text-3xl'>
                   {profile.firstName} {profile.lastName}
                 </p>
@@ -78,12 +102,32 @@ export default function ProfilePage(props: InferPageProps<ProfilesController, 'i
                     </Dialog>
                   </div>
                 </div>
+                {profileEditions.length > 0 ?
+                  <div className='flex flex-row flex-wrap gap-2 justify-center md:justify-normal'>
+                    {profileEditions.map((edition) => (
+                      <Badge>
+                        {edition}
+                      </Badge>
+                    ))}
+                  </div>
+                  :
+                  <div>
+                    <Badge variant={'secondary'}>
+                      Primeiro ENEI
+                    </Badge>
+                  </div>
+                }
               </div>
             </section>
-            <section>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+            <section className='grid grid-rows-[auto_1fr] gap-4 mt-4'>
+              <div>
+                <h4 className='font-bold text-lg text-center md:text-left'>
+                  Sobre
+                </h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
             </section>
             {isUser &&
               <section>
