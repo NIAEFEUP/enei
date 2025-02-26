4a849501d7033064db56ff3fba8fc5af6b2bc138
Tomás Palma <up202108880@edu.fe.up.pt>
2025-02-26 15:45:33 +0000

    feat: point product order now has initial pending state

--- website/inertia/components/common/navbar.tsx
+++ website/inertia/components/common/navbar.tsx
@@ -99,7 +99,7 @@ export function Navbar({ className, variant = "blue" }: { className?: string, va
             </Link>
             <div className='flex gap-4 items-center justify-between'>
               <div className={auth.state === 'authenticated' ? 'block' : 'hidden'}>
-                <Link route="pages:store" className={cn("", `text-${textColor}`)}>
+                <Link route="pages:store" className={cn(buttonVariants({variant: 'link'}), `text-${textColor}`)}>
                     <span>Loja</span>
                 </Link>
               </div>
