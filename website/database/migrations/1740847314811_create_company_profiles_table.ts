4982752ee783111dfea8f5fca13aaea5e6263f54
Hugo Almeida <1241273@isep.ipp.pt>
2025-03-01 22:39:47 +0000

    feat: added migrations to new models

---
+++ website/database/migrations/1740847314811_create_company_profiles_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'company_profiles'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.timestamps({defaultToNow: true})
+
+      table.string('name').notNullable()
+      table.string('logo').notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
