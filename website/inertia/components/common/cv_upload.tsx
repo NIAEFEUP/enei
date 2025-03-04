4f251e8293db9ebac9cc8fdd97cd67223787fb72
AvilaAndre <avila_andre14@hotmail.com>
2025-03-04 16:16:29 +0000

    refactor: change indentation to follow project's indentation

--- website/inertia/components/common/cv_upload.tsx
+++ website/inertia/components/common/cv_upload.tsx
@@ -6,87 +6,87 @@ import { useEffect } from 'react';
 import { useTuyau } from '~/hooks/use_tuyau'
 
 const CvUpload = () => {
-    const [file, setFile] = useState<File | null>(null);
-    const [uploading, setUploading] = useState(false);
-    const [fileName, setFileName] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const [uploading, setUploading] = useState(false);
+  const [fileName, setFileName] = useState<string | null>(null);
 
-    const tuyau = useTuyau()
+  const tuyau = useTuyau()
 
-    useEffect(() => {
-        const fetchFileName = async () => {
-            try {
-                const response = await axios.get(tuyau.$url('actions:cv.name'));
-                setFileName(response.data.fileName);
-            } catch (error) {
-                setFileName(null);
-            }
-        };
-
-        fetchFileName();
-    }, [uploading]);
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files.length > 0) {
-            setFile(event.target.files[0]);
-        }
+  useEffect(() => {
+    const fetchFileName = async () => {
+      try {
+        const response = await axios.get(tuyau.$url('actions:cv.name'));
+        setFileName(response.data.fileName);
+      } catch (error) {
+        setFileName(null);
+      }
     };
 
-    const handleUpload = async () => {
-        if (!file) {
-            return;
-        }
+    fetchFileName();
+  }, [uploading]);
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFile(event.target.files[0]);
+    }
+  };
 
-        setUploading(true);
-
-        const formData = new FormData();
-        formData.append('cv', file);
-
-        try {
-            await axios.post(tuyau.$url('actions:cv.upload'), formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-        } catch (error) {
-        } finally {
-            setUploading(false);
-        }
-    };
-
-    const handleDelete = async () => {
-        setUploading(true);
-        try {
-            await axios.delete(tuyau.$url('actions:cv.delete'), {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-
-            setFile(null)
-        } catch (error) {
-        } finally {
-            setUploading(false);
-        }
+  const handleUpload = async () => {
+    if (!file) {
+      return;
     }
 
-    return (
-        <div className='flex flex-col gap-2'>
-            {fileName ? (
-                <div className='flex flex-row gap-2'>
-                    <Input className="w-64" type="text" value={fileName} disabled />
-                    <Button onClick={handleDelete} disabled={uploading} variant={'destructive'}>
-                        {uploading ? 'Uploading...' : 'Clear CV'}
-                    </Button>
-                </div>
-            ) : (
-                <div className='flex flex-row gap-2 '>
-                    <Input className="w-64" type="file" accept=".pdf" onChange={handleFileChange} />
-                    <Button onClick={handleUpload} disabled={uploading || !file}>
-                        {uploading ? 'Uploading...' : 'Upload CV'}
-                    </Button>
-                </div>
-            )}
+    setUploading(true);
+
+    const formData = new FormData();
+    formData.append('cv', file);
+
+    try {
+      await axios.post(tuyau.$url('actions:cv.upload'), formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+    } catch (error) {
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  const handleDelete = async () => {
+    setUploading(true);
+    try {
+      await axios.delete(tuyau.$url('actions:cv.delete'), {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      setFile(null)
+    } catch (error) {
+    } finally {
+      setUploading(false);
+    }
+  }
+
+  return (
+    <div className='flex flex-col gap-2'>
+      {fileName ? (
+        <div className='flex flex-row gap-2'>
+          <Input className="w-64" type="text" value={fileName} disabled />
+          <Button onClick={handleDelete} disabled={uploading} variant={'destructive'}>
+            {uploading ? 'Uploading...' : 'Clear CV'}
+          </Button>
         </div>
-    );
+      ) : (
+        <div className='flex flex-row gap-2 '>
+          <Input className="w-64" type="file" accept=".pdf" onChange={handleFileChange} />
+          <Button onClick={handleUpload} disabled={uploading || !file}>
+            {uploading ? 'Uploading...' : 'Upload CV'}
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CvUpload;
