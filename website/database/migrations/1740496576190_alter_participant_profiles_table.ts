28b5f694f203e5bf9c2c9b45f089ad3bc693ec71
André Lima <me@limwa.pt>
2025-03-05 12:02:00 +0000

    fix: migrations for production

--- website/database/migrations/1740496576190_alter_participant_profiles_table.ts
+++ website/database/migrations/1740496576190_alter_participant_profiles_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.alterTable(this.tableName, (table) => {
-      table.string('slug').unique().notNullable()
+      table.string('slug').unique()
       table.string('about')
       table.string('github')
       table.string('linkedin')
