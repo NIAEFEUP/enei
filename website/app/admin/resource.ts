70c5d22eee5a731ea478958c52252051b7e1eff6
toino <me@toino.pt>
2025-03-15 04:36:53 +0000

    Add helpers for adminjs resources and relations

---
+++ website/app/admin/resource.ts
@@ -0,0 +1,71 @@
+import {
+  flat,
+  mergeResourceOptions,
+  type FeatureType,
+  type ResourceOptions,
+  type ResourceWithOptions,
+} from 'adminjs'
+import type { LucidModel } from '@adonisjs/lucid/types/model'
+import dbConfig from '#config/database'
+import { LucidResource } from '@adminjs/adonis'
+import { DateTime } from 'luxon'
+
+export const createResource = ({
+  model,
+  options,
+  features,
+}: {
+  model: LucidModel
+  options?: ResourceOptions
+  features?: FeatureType[]
+}) =>
+  ({
+    resource: new LucidResource(model, dbConfig.connection),
+    features,
+    options: mergeResourceOptions(
+      {
+        actions: {
+          edit: {
+            before: async (request, context) => {
+              if (request.payload === undefined) return request
+
+              for (const property of context.resource.properties()) {
+                let type = property.type()
+
+                if (type === 'date' || type === 'datetime') {
+                  // @ts-expect-error: This is cringe but it's the only way to use luxon's DateTime
+                  property.type = () => `_${type}`
+                  type = property.type()
+                }
+
+                if (
+                  // @ts-expect-error: See above
+                  (type === '_date' || type === '_datetime') &&
+                  flat.get(request.payload, property.path())
+                )
+                  flat.set(
+                    request.payload,
+                    property.path(),
+                    DateTime.fromISO(flat.get(request.payload, property.path()))
+                  )
+              }
+
+              delete request.payload.createdAt
+              delete request.payload.updatedAt
+
+              return request
+            },
+          },
+        },
+        properties: {
+          createdAt: {
+            isVisible: { list: true, filter: true, show: true, edit: false },
+          },
+          updatedAt: {
+            isVisible: { list: true, filter: true, show: true, edit: false },
+          },
+        },
+      },
+      options
+    ),
+  }) satisfies ResourceWithOptions
