65c6328629ecc3fd53e4a6ef13782be294fea058
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-03-19 18:02:50 +0000

    fix: formatting

--- website/app/middleware/wip_middleware.ts
+++ website/app/middleware/wip_middleware.ts
@@ -4,11 +4,11 @@ import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 
 export default class WipMiddleware {
-  async handle({request}: HttpContext, next: NextFn) {
+  async handle({ request }: HttpContext, next: NextFn) {
     if (app.inDev) {
       return next()
     }
-    
+
     throw new errors.E_ROUTE_NOT_FOUND([request.method(), request.url()])
   }
 }
