6462de21f83e6b2a778c134ed988d28d2831de1e
Pedro Silva <pedroafsuper@gmail.com>
2025-03-25 17:32:28 +0000

    refactor: move order payment validation to order services

--- apps/website/app/controllers/orders_controller.ts
+++ apps/website/app/controllers/orders_controller.ts
@@ -2,39 +2,31 @@ import type { HttpContext } from "@adonisjs/core/http";
 import env from "#start/env";
 import axios from "axios";
 import Order from "#models/order";
-import User from "#models/user";
 import OrderProduct from "#models/order_product";
 import Product from "#models/product";
 import ProductGroup from "#models/product_group";
 import { createMBWayOrderValidator } from "#validators/order";
 import UpdateOrderStatus from "../jobs/update_order_status.js";
+import { OrderService } from "#services/order_service";
+import { inject } from "@adonisjs/core";
+
+@inject()
 export default class OrdersController {
+  constructor(private orderService: OrderService) {}
   index({ inertia }: HttpContext) {
     return inertia.render("payments");
   }
 
   public async createMBWay({ request, auth, response }: HttpContext) {
     const authUser = auth.user;
-
+    if (!authUser) {
+      return response.status(401).json({ message: "Não autorizado" });
+    }
     try {
       // Validate input format
       await request.validateUsing(createMBWayOrderValidator);
 
-      const { userId, products, name, nif, address, mobileNumber } = request.all();
-
-      // Validate authentication
-
-      if (!authUser || authUser.id !== userId) {
-        return response.status(401).json({ message: "Não autorizado" });
-      }
-
-      // Validate user existence
-
-      const user = await User.find(userId);
-
-      if (!user) {
-        return response.status(404).json({ message: "Utilizador não encontrado" });
-      }
+      const { products, name, nif, address, mobileNumber } = request.all();
 
       let totalAmount = 0;
       let description = "";
@@ -51,58 +43,23 @@ export default class OrdersController {
             .json({ message: `Produto com id ${productId} não foi encontrado` });
         }
 
-        const successfulOrdersOfGivenProduct = await OrderProduct.query()
-          .join("orders", "order_products.order_id", "orders.id")
-          .where("order_products.product_id", productId)
-          .whereIn("orders.status", ["Success", "Pending"]);
-
-        const successfulOrdersOfGivenProductPerUser = await OrderProduct.query()
-          .join("orders", "order_products.order_id", "orders.id")
-          .where("orders.user_id", userId)
-          .where("order_products.product_id", productId)
-          .whereIn("orders.status", ["Success", "Pending"]);
-
-        const stockUsed = successfulOrdersOfGivenProduct.reduce(
-          (acc, orderProduct) => acc + orderProduct.quantity,
-          0,
-        );
-
-        const totalQuantity = successfulOrdersOfGivenProductPerUser.reduce(
-          (acc, orderProduct) => acc + orderProduct.quantity,
-          0,
-        );
-
-        if (product.stock < quantity + stockUsed) {
+        if (!(await this.orderService.checkProductStock(product, quantity))) {
           return response
             .status(400)
             .json({ message: `Não há mais stock do produto ${product.name}` });
         }
 
-        if (quantity + totalQuantity > product.maxOrder) {
+        if (!(await this.orderService.checkUserMaxOrders(auth.user, product))) {
           return response.status(400).json({
             message: `Apenas podes comprar ${product.maxOrder} do produto ${product.name}`,
           });
         }
 
-        const productGroup = await ProductGroup.find(product.productGroupId);
-        if (productGroup) {
-          const sucessfulOrdersOfGivenGroup = await OrderProduct.query()
-            .join("orders", "order_products.order_id", "orders.id")
-            .join("products", "order_products.product_id", "products.id")
-            .where("orders.user_id", userId)
-            .where("products.product_group_id", product.productGroupId)
-            .where("orders.status", "Success");
-
-          const totalGroupQuantity = sucessfulOrdersOfGivenGroup.reduce(
-            (acc, orderProduct) => acc + orderProduct.quantity,
-            0,
-          );
-
-          if (totalGroupQuantity + quantity > productGroup.maxAmountPerGroup) {
-            return response.status(400).json({
-              message: `Apenas podes comprar ${productGroup?.maxAmountPerGroup} produtos do grupo ${productGroup.name}`,
-            });
-          }
+        if (!(await this.orderService.checkUserMaxGroupOrders(auth.user, product, quantity))) {
+          const productGroup = await ProductGroup.find(product.productGroupId);
+          return response.status(400).json({
+            message: `Apenas podes comprar ${productGroup?.maxAmountPerGroup} produtos do grupo ${productGroup?.name}`,
+          });
         }
         productDetails.push({ product, quantity });
         totalAmount += product.price * quantity;
@@ -112,7 +69,7 @@ export default class OrdersController {
       description = `Payment for order: ${description.slice(0, -2)}`;
 
       // Create the order and associated products
-      const order = await Order.create({ userId, name, nif, address });
+      const order = await Order.create({ userId: authUser.id, name, nif, address });
 
       for (const { product, quantity } of productDetails) {
         await OrderProduct.create({
@@ -146,7 +103,7 @@ export default class OrdersController {
         // Dispatch background job to update order status
 
         await UpdateOrderStatus.dispatch(
-          { requestId: order.requestId, email: authUser.email },
+          { requestId: order.requestId, email: auth.user.email },
           { delay: 10000 },
         ).catch((error) => {
           console.error("Error dispatching job", error);
