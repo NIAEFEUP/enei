2159ccf964fea7b9e6bf44e0f1d792b22be2c82c
Pedro Silva <pedroafsuper@gmail.com>
2025-03-25 17:39:54 +0000

    refactor: use Bouncer to authorize referral services

---
+++ apps/website/app/policies/user_policy.ts
@@ -0,0 +1,20 @@
+import User from "#models/user";
+import { BasePolicy } from "@adonisjs/bouncer";
+
+export default class UserPolicy extends BasePolicy {
+  /**
+   * Only promoters or users with purchased tickets can refer other users
+   */
+  async refer(user: User) {
+    if (user.isPromoter()) return true;
+
+    return await User.hasPurchasedTicket(user);
+  }
+
+  /**
+   * Only users who are not promoters, have not been referred, and have not purchased tickets can be linked as referrals
+   */
+  async beLinked(user: User) {
+    return !user.isPromoter() && !user.wasReferred() && !(await User.hasPurchasedTicket(user));
+  }
+}
