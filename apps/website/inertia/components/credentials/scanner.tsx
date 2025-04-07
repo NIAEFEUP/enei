bf710c41f50ebc47d4096db3fb068ddb4e3c7761
toino <me@toino.pt>
2025-04-07 15:22:53 +0100

    Add nfc option to credential scanner

--- apps/website/inertia/components/credentials/scanner.tsx
+++ apps/website/inertia/components/credentials/scanner.tsx
@@ -1,4 +1,6 @@
 import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import NFC from "./nfc";
 
 interface CredentialScannerProps {
   onScan: (slug: string) => void;
@@ -6,18 +8,37 @@ interface CredentialScannerProps {
 
 function CredentialScanner({ onScan }: CredentialScannerProps) {
   return (
-    <Scanner
-      key={"credential-scanner"}
-      onScan={(data: IDetectedBarcode[]) => {
-        const value = data[0].rawValue.split("/");
-        const slug = value[value.length - 1];
+    <Tabs defaultValue="qr">
+      <TabsList className="w-full">
+        <TabsTrigger value="qr">QR Code</TabsTrigger>
+        <TabsTrigger value="nfc">NFC</TabsTrigger>
+      </TabsList>
+      <TabsContent value="qr">
+        <Scanner
+          key={"credential-scanner"}
+          onScan={(data: IDetectedBarcode[]) => {
+            const value = data[0].rawValue.split("/");
+            const slug = value[value.length - 1];
 
-        onScan(slug);
-      }}
-      components={{ audio: false }}
-      allowMultiple={true}
-      scanDelay={0}
-    />
+            onScan(slug);
+          }}
+          components={{ audio: false }}
+          allowMultiple={true}
+          scanDelay={0}
+        />
+      </TabsContent>
+      <TabsContent value="nfc">
+        <NFC
+          onRead={(data) => {
+            const decoder = new TextDecoder();
+            const value = decoder.decode(data.records[0].data).split("/");
+            const slug = value[value.length - 1];
+
+            onScan(slug);
+          }}
+        />
+      </TabsContent>
+    </Tabs>
   );
 }
 
