93568e132b9bfbbcc05d5df461ef7933c6049fc2
limwa <limwa@users.noreply.github.com>
2025-04-10 18:53:40 +0000

    [no ci] style: format code

--- apps/website/app/services/points_service.ts
+++ apps/website/app/services/points_service.ts
@@ -27,14 +27,16 @@ export default class PointsService {
     if (referralDescription.referralIsPromoter) {
       await db.transaction(async (trx) => {
         const user = await User.findOrFail(referralDescription.referralUserId, { client: trx });
-        
+
         user.points +=
           PointsService.pointsRegistry.get(UserActivityType.Referral)?.["promoter"] ?? 0;
         await user.save();
       });
     } else {
       await db.transaction(async (trx) => {
-        const referralUser = await User.findOrFail(referralDescription.referralUserId, { client: trx });
+        const referralUser = await User.findOrFail(referralDescription.referralUserId, {
+          client: trx,
+        });
         referralUser.points +=
           PointsService.pointsRegistry.get(UserActivityType.Referral)?.["participant"] ?? 0;
         await referralUser.save();
