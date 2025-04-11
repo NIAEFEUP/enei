2ef1c511daad31e86f58c06a93a99ea3fa8d7681
André Lima <me@limwa.pt>
2025-04-11 09:48:16 +0100

    fix: remove duplicate migrations

--- apps/website/database/migrations/1744270856201_alter_users_table.ts
+++ apps/website/database/migrations/1744270856201_alter_users_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.alterTable(this.tableName, (table) => {
-      table.string("slug").nullable();
+      table.string("slug");
       table
         .integer("speaker_profile_id")
         .references("id")
