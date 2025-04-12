b3dca7b031419b7eb7a6fb894d027797d506717d
André Lima <me@limwa.pt>
2025-04-12 14:18:13 +0100

    only show cv if present

--- apps/website/inertia/components/companies/participants/participants_table.tsx
+++ apps/website/inertia/components/companies/participants/participants_table.tsx
@@ -155,9 +155,9 @@ const columns: ColumnDef<Participant>[] = [
               {liked ? <HeartOff className="mr-2 h-4 w-4" /> : <Heart className="mr-2 h-4 w-4" />}
               Like
             </DropdownMenuItem>
-            <DropdownMenuItem className="flex flex-row">
+            {row.original.cvLink && (<DropdownMenuItem className="flex flex-row">
               <a
-                href={row.original.cvLink || "#"}
+                href={row.original.cvLink}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex flex-row"
@@ -165,7 +165,7 @@ const columns: ColumnDef<Participant>[] = [
                 <FileUser className="mr-2 h-4 w-4" />
                 CV
               </a>
-            </DropdownMenuItem>
+            </DropdownMenuItem>)}
           </DropdownMenuContent>
         </DropdownMenu>
       );
