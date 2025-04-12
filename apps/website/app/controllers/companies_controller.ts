abb03262744d1c2d6aae4782c2362c9a213a4b51
DGoiana <diogogoianam@gmail.com>
2025-04-12 10:38:04 +0100

    adding visited users

--- apps/website/app/controllers/companies_controller.ts
+++ apps/website/app/controllers/companies_controller.ts
@@ -1,6 +1,7 @@
 import type { HttpContext } from "@adonisjs/core/http";
 import { inject } from "@adonisjs/core";
 import User from "#models/user";
+import Event from "#models/event"
 import { UserActivityService } from "#services/user_activity_service";
 
 @inject()
@@ -96,9 +97,50 @@ export default class CompaniesController {
           };
         }),
       );
+
+      const event = await Event.query()
+        .where("companyId", companyUser.representativeProfile?.companyId)
+        .preload("checkedInUsers", (user) => {
+          user.preload("participantProfile")
+        })
+        .first()
+
+
+        const checkedParticipants = await Promise.all(
+          event?.checkedInUsers.map(async (participant) => {
+            const likes = await this.userActivityService.getCompanyLikes(
+              participant.id,
+              companyUser.representativeProfile?.companyId,
+            );
+            const likedBy = await Promise.all(
+              likes.map(async (user) => {
+                if (!user.representativeProfileId) {
+                  return null;
+                }
+                await user.load("representativeProfile");
+                return (
+                  user.representativeProfile.firstName + " " + user.representativeProfile.lastName
+                );
+              }),
+            );
+        
+            return {
+              id: participant.id,
+              name: `${participant.participantProfile.firstName} ${participant.participantProfile.lastName}`,
+              photoUrl: null, // TODO: add photo when available
+              faculty: participant.participantProfile.university,
+              course: participant.participantProfile.course,
+              year: participant.participantProfile.curricularYear,
+              cvLink: null, // TODO: add when cv is available
+              likedBy: likedBy.filter((name) => name !== null),
+              isLiked: await this.userActivityService.isLiked(participant.id, companyUser.id),
+            };
+          }) || []
+        );
+
     return inertia.render("company/participants", {
       allParticipants: allParticipants,
-      checkedParticipants: [],
+      checkedParticipants: checkedParticipants,
       likedParticipants: allParticipants.filter((p) => p.isLiked),
     });
   }
