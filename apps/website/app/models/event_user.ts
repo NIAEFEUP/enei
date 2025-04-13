135f7a084b3ab7cdfa77382426c6eb213340a32b
André Lima <me@limwa.pt>
2025-04-13 04:39:05 +0100

    fix: make table name explicit

--- apps/website/app/models/event_user.ts
+++ apps/website/app/models/event_user.ts
@@ -2,6 +2,8 @@ import { BaseModel, column } from "@adonisjs/lucid/orm";
 import type { DateTime } from "luxon";
 
 export default class EventUser extends BaseModel {
+  static table = "event_users";
+  
   @column({ isPrimary: true })
   declare id: number;
 
