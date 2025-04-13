ae63e255eb450e98194ecafd363f7e4d3db221a3
Tomás Palma <up202108880@edu.fe.up.pt>
2025-04-13 11:10:50 +0100

    chore: add new logos

--- apps/website/inertia/components/home/partners/sponsors.tsx
+++ apps/website/inertia/components/home/partners/sponsors.tsx
@@ -2,7 +2,7 @@ export default function Sponsors() {
   return (
     <section className="flex w-full flex-col items-center gap-y-8">
       <h3 className="text-enei-blue text-center text-3xl font-bold lowercase">Patrocínios</h3>
-      <div className="grid grid-cols-3 items-center gap-x-8 gap-y-8 md:grid-cols-3">
+      <div className="grid grid-cols-2 items-center gap-x-8 gap-y-8 md:grid-cols-2">
         <img className="w-24" src="/images/company/dominos.png" alt="Dominos logo" />
 
         <img className="w-24" src="/images/company/nicola.png" alt="Nicola logo" />
@@ -14,6 +14,8 @@ export default function Sponsors() {
         <img className="w-24" src="/images/company/mauser.png" alt="Mauser logo" />
 
         <img className="w-24" src="/images/company/kevel.png" alt="Kevel logo" />
+
+        <img className="w-24" src="/images/company/saborosa.png" alt="Saborosa logo" />
       </div>
     </section>
   );
