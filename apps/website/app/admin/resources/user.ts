c903118d3e20e9ccad628e116479f496734c1b48
toino <me@toino.pt>
2025-04-13 07:07:15 +0100

    Add action to recalculate user points

--- apps/website/app/admin/resources/user.ts
+++ apps/website/app/admin/resources/user.ts
@@ -1,6 +1,10 @@
 import User from "#models/user";
 import { createResource } from "../resource.js";
 import { owningRelationFeature, targetRelationFeature } from "../relations.js";
+import db from "@adonisjs/lucid/services/db";
+import OrderProduct from "#models/order_product";
+import Order from "#models/order";
+import Event from "#models/event";
 
 const UserResource = createResource({
   model: User,
@@ -23,6 +27,113 @@ const UserResource = createResource({
       },
       referrerId: {
         reference: "users",
+      },
+    },
+    actions: {
+      recalculatePoints: {
+        actionType: "record",
+        component: false,
+        variant: "danger",
+        handler: async (request, _, context) => {
+          const trx = await db.transaction({ isolationLevel: "serializable" });
+
+          const userId = request.params.recordId;
+
+          if (!userId) {
+            return;
+          }
+
+          await OrderProduct.query({ client: trx })
+            .delete()
+            .whereIn("order_id", Order.query().select("id").where("user_id", userId))
+            .andWhereIn(
+              "product_id",
+              Event.query().select("id").distinct().whereNotNull("product_id"),
+            );
+
+          await trx.rawQuery(
+            `
+            with product_requirements as (
+              select
+                events.product_id as product_id,
+                count(*) as quantity,
+                ROW_NUMBER() over (order by events.product_id) as rn
+              from events join event_users on event_users.event_id = events.id
+              where event_users.user_id = ? and events.product_id is not null
+              group by events.product_id
+            ),
+            new_orders_without_rn as (
+              insert into orders (status, user_id, created_at, updated_at, points_used)
+              select 'delivered', ?, NOW(), NOW(), 0
+              from product_requirements
+              returning id
+            ),
+            new_orders as (
+              select
+                new_orders_without_rn.*,
+                ROW_NUMBER() over () as rn
+              from new_orders_without_rn
+            )
+            insert into order_products (order_id, product_id, quantity, created_at, updated_at)
+            select
+              new_orders.id as order_id,
+              product_requirements.product_id as product_id,
+              product_requirements.quantity as quantity,
+              NOW() as created_at,
+              NOW() as updated_at
+            from new_orders
+            join product_requirements on product_requirements.rn = new_orders.rn;
+          `,
+            [userId, userId],
+          );
+
+          await Order.query({ client: trx })
+            .delete()
+            .where("user_id", userId)
+            .andWhereIn(
+              "id",
+              trx.raw(`
+                select orders.id
+                except select order_id
+                from order_products
+                group by order_id
+                having count(*) > 0
+              `),
+            );
+
+          await trx.rawQuery(
+            `
+              update orders
+              set points_used = (
+                select COALESCE(sum(products.points * order_products.quantity), 0) as total_points
+                from order_products join products on products.id = order_products.product_id and order_products.order_id = orders.id
+              )
+              where orders.user_id = ?
+            `,
+            [userId],
+          );
+
+          const {rows: [{points}]} = await trx.rawQuery(
+            `
+              update users
+              set points = (
+                select COALESCE(sum(orders.points_used), 0) as total_points
+                from orders where orders.user_id = users.id
+              )
+              where id = ?
+              returning points
+            `,
+            [userId],
+          );
+
+          await trx.commit();
+
+          context.record?.set("points", points);
+
+          return {
+            record: context.record?.toJSON(context.currentAdmin),
+          }
+        },
       },
     },
   },
