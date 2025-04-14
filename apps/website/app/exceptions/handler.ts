33abe78f4f3bcd6d5eb8937861157aca05e38172
André Lima <me@limwa.pt>
2025-04-14 11:39:44 +0100

    debug pls

--- apps/website/app/exceptions/handler.ts
+++ apps/website/app/exceptions/handler.ts
@@ -7,14 +7,14 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * In debug mode, the exception handler will display verbose errors
    * with pretty printed stack traces.
    */
-  protected debug = !app.inProduction;
+  protected debug = true;//!app.inProduction;
 
   /**
    * Status pages are used to display a custom HTML pages for certain error
    * codes. You might want to enable them in production only, but feel
    * free to enable them in development as well.
    */
-  protected renderStatusPages = app.inProduction;
+  protected renderStatusPages = false; //app.inProduction;
 
   protected ignoreCodes = ["E_AUTH_DISABLED"];
 
